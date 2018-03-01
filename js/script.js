4c613a17a8c0dc978c5f6ffafa08098c1f14bac4
MatWidOZS <36623352+MatWidOZS@users.noreply.github.com>
2018-03-01 19:50:10 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -8,11 +8,11 @@ function getTriangleArea(a, h) {
 
 console.log(getTriangleArea(10, 6));
 
-var triangle1Area = getTriangleArea(10, 15);
+var triangle1Area = getTriangleArea(10, -15);
 console.log('pole pierwszego trójkąta: ' + triangle1Area);
 
 var	triangle2Area = getTriangleArea(0, 20);
 console.log('pole drugiego trójkąta: ' + triangle2Area);
 
-var	triangle3Area = getTriangleArea(2.5, 8);
+var	triangle3Area = getTriangleArea(-2.5, 8);
 console.log('pole trzeciego trójkąta: ' + triangle3Area);
